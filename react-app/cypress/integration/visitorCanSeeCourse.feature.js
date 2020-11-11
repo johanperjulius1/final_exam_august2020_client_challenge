f85af5e541cd6ec72ed72f1efaf3141dfe4d0284
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-11-11 13:42:14 +0100

    Creating a toDo list. Moving tests outside of beforeEach.

--- react-app/cypress/integration/visitorCanSeeCourse.feature.js
+++ react-app/cypress/integration/visitorCanSeeCourse.feature.js
@@ -2,24 +2,23 @@ describe("visitor can see course list", () => {
   context("", () => {
     beforeEach(() => {
       cy.visit("http://localhost:3000");
-
-      it("displays first article", () => {
-        cy.get("#article-1").within(() => {
-          cy.get("h1").should("contain", "WORK THE WEB");
-        });
+    });
+    it("displays first article", () => {
+      cy.get("#article-1").within(() => {
+        cy.get("h1").should("contain", "WORK THE WEB");
       });
+    });
 
-      it("displays second article", () => {
-        cy.get("#article-2").within(() => {
-          cy.get("p").should("contain", "Cypress is a cutting-edge");
-        });
+    it("displays second article", () => {
+      cy.get("#article-2").within(() => {
+        cy.get("p").should("contain", "Cypress is a cutting-edge");
       });
+    });
 
-      it("displays third article", () => {
-        cy.get("#article-3").within(() => {
-          cy.get(".capitalized").within(() => {
-            cy.get(".bold").should("Vue");
-          });
+    it("displays third article", () => {
+      cy.get("#article-3").within(() => {
+        cy.get(".capitalized").within(() => {
+          cy.get(".bold").should("Vue");
         });
       });
     });
